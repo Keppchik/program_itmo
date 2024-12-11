6f9dbaf7bf6f45af0157ef7d9ba6649ee4edefae
Keppchik <466353@niuitmo.ru>
2024-12-12 00:38:56 +0300

    Сделал тесты для лабораторной

--- src/lab4/ClassOrder.py
+++ src/lab4/ClassOrder.py
@@ -1,5 +1,7 @@
 class Order:
     def __init__(self, data):
+        """Инициализирует объект Order"""
+
         self.number = data[0]
         self.products = data[1]
         self.compacted_products = {}
@@ -13,9 +15,18 @@ class Order:
         self.compact_products()
 
     def is_valid(self):
+        """Проверяем есть ли ошибки в номере телефона и в адресе заказа"""
+
         phone_split = self.phone.split("-")
-        if len(phone_split) < 5 or self.phone[0] != "+" or len(phone_split[0]) != 2 or len(phone_split[1]) != 3 or len(
-                phone_split[2]) != 3 or len(phone_split[3]) != 2 or len(phone_split[4]) != 2:
+        if (
+            len(phone_split) < 5
+            or self.phone[0] != "+"
+            or len(phone_split[0]) != 2
+            or len(phone_split[1]) != 3
+            or len(phone_split[2]) != 3
+            or len(phone_split[3]) != 2
+            or len(phone_split[4]) != 2
+        ):
             if self.phone == "":
                 self.errors.append((2, "no data"))
             else:
@@ -28,6 +39,8 @@ class Order:
                 self.errors.append((1, self.adress))
 
     def compact_products(self):
+        """Создаем словарь где каждому продукту соответствует его количество"""
+
         products_split = self.products.split(", ")
         for product in products_split:
             if product not in self.compacted_products:
@@ -36,4 +49,4 @@ class Order:
                 self.compacted_products[product] += 1
 
     def __str__(self):
-        return f"{self.number}, {self.products},{self.customer}, {self.adress}, {self.phone}, {self.priority}{self.errors}"
+        return f"{self.number};{self.products};{self.customer};{self.adress};{self.phone};{self.priority};{self.errors}"
